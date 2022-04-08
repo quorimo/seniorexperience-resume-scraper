45a5e99d692eaca2646fefe7abca49566e092f4c
quorimo <jimmolee1226@gmail.com>
2022-04-08 10:32:37 -0400

    naming fixed

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-import glob 
 import os 
-
+import time
 #######################################################
 """ LOAD ENVIRONMENT VARIABLES """ 
 load_dotenv()
@@ -29,8 +28,6 @@ chrome_options.add_experimental_option("prefs",prefs)
 
 driver = webdriver.Chrome(service=s, options=chrome_options)
 
-glob_path = download_dir + "/*"
-
 ######################################################
 """ OPEN PAGE AND LOGIN """ 
 
@@ -43,28 +40,35 @@ driver.find_element(By.ID, 'login-password').send_keys(password)
 """ OPEN PROFILE PAGES AND DOWNLOAD FILES """ 
 counter = 0 
 
-for i in range (1255, 1400):
-  try:
-    # go to link
+for i in range (1255, 1400): # these numbers are just from what i can tell, may not include all signed up students
+  try:  
+  # go to link
     driver.get('https://www.bcaseniorexperience.org/resume/' + str(i))
     
     # get student name 
     name = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/h1').text
     name = name.replace(' ', '')
 
-    # click on download for resume
+    # predownload
+    before = os.listdir(download_dir) 
+    
+    # download
     driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[1]/div/ul/li[2]/a').click()
-    time.sleep(5)
-
-    # nice naming for resume files 
-    file_list = glob.glob(glob_path)
-    latest_file = max(file_list, key=os.path.getctime)
+    time.sleep(0.5) # try testing different numbers  
+    
+    # renaming files 
+    after = os.listdir(download_dir) 
+    change = set(after) - set(before) 
+    while(len(change)==0):
+      pass
+    if len(change)==1:
+      latest_file = download_dir + change.pop() 
     a = latest_file.split(".")
-    # check for duplicates 
-    new_path = resumes_dir + name + "." +  a[len(a)-1]
+    new_path = download_dir + name + "." +  a[len(a)-1]
     os.rename(latest_file, new_path)
-    time.sleep(3) 
+
   except:
+    # yeah this logging thing is still not working correctly
     log = open("log.txt", "a") 
     log.write(str(i) + " doesn't exist or does not have a resume.\n") 
     log.close()
